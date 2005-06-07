c01fb72684f4f930b38130e7c280cabbe36a420b
Aaron Klingaman <alk@cs.princeton.edu>
2005-06-07 15:14:07 +0000

    final 3.0 image

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ CONFIGURATIONS_DIR=configurations/
 # where built files are stored
 BUILD_DIR=build/
 
-BOOTCD_VERSION="3.0-beta0.5"
+BOOTCD_VERSION="3.0"
 FULL_VERSION_STRING="PlanetLab BootCD"
 OUTPUT_IMAGE_NAME='PlanetLab-BootCD'
     
