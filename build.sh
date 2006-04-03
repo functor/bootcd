441d26295bed73eeda18f96abc56f665ef52f5a5
Mark Huang <mlhuang@cs.princeton.edu>
2006-04-03 20:07:45 +0000

    - define default PATH

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@
 # Mark Huang <mlhuang@cs.princeton.edu>
 # Copyright (C) 2004-2006 The Trustees of Princeton University
 #
-# $Id: build.sh,v 1.5 2006/03/29 17:08:45 mlhuang Exp $
+# $Id: build.sh,v 1.32 2006/04/03 19:33:56 mlhuang Exp $
 #
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 CONFIGURATION=default
 NODE_CONFIGURATION_FILE=
